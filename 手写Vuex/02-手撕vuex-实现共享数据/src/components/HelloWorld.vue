e0e8f546bcc86fcc3ead6a73fed50258b2ca0f9e
AzrDream <1499325862@qq.com>
2022-07-27 20:37:19 +0800

    手写Vuex-实现共享数据

---
+++ 手写Vuex/02-手撕vuex-实现共享数据/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+        <input type="text" v-model="this.$store.state.name">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    mounted(){
+      console.log(this.$store);
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
