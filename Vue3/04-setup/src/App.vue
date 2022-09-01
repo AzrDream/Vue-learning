06f1420b8ff2684b8a3394994867191e97b76616
AzrDream <1499325862@qq.com>
2022-09-01 10:17:48 +0800

    初识setup

---
+++ Vue3/04-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{name}}</h1>
+  <h1>{{age}}</h1>
+  <button @click="say">按钮</button>
+</template>
+
+<script>
+  import { h } from 'vue'
+  export default {
+    name: 'App',
+    // 此处只是设置setup，不考虑响应式
+    setup() {
+      // 数据
+      /*
+      let name = 'azrdream'
+      let age = 22*/
+
+      // 方法
+      /*
+      function say() {
+        console.log(`${name},${age}`);
+      }*/
+
+      // 返回一个渲染函数
+      return ()=>h('h1','azrdream')
+      // 返回一个对象
+      /*
+      return {
+        name,
+        age,
+        say
+      }
+       */
+    }
+  }
+</script>
+
+<style>
+</style>
